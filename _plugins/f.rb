6fef62b62b3fb1279e12b6eb8dfa67e668e72ba2
Jason Mitchell <jason@stderr.me>
2013-09-07 00:01:13 -0400

    / ‘_plugins/f.rb’

--- _plugins/f.rb
+++ _plugins/f.rb
@@ -1,3 +1,14 @@
+require 'liquid'
+require 'json'
+require 'flickraw-cached'
+
+FlickRaw.secure = true
+
+FlickRaw.api_key = ENV['FLICKR_API_KEY']
+FlickRaw.shared_secret = ENV['FLICKR_SHARED_SECRET']
+flickr.access_token = ENV['FLICKR_AUTH_TOKEN']
+flickr.access_secret = ENV['FLICKR_AUTH_SECRET']
+
 module Jekyll
   class RenderTimeTag < Liquid::Tag
 
